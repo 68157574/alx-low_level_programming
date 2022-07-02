bdbd9690e0643bcd012113200461bfecec1140ec
68157574 <abebemillion999@gmail.com>
2022-07-02 04:26:11 -0700

    0k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc 4-puts.c
+#include <stdio.h>
+/**
+*main -prints the size of various types
+*Return: 0 if exited properly,no-zero otherwise
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
