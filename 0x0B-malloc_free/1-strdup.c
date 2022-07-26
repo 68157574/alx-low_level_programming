7a4b43cddd753d9136d56e22f1142768776b3131
68157574 <abebemillion999@gmail.com>
2022-07-26 05:54:58 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,7 +4,6 @@
 
 
 /**
-
 * _strdup - function to return ptr containing cpy of string
 * @str: string to be copied
 * Return: Null for failure else return ptr.
@@ -18,7 +17,7 @@ return (NULL);
 for (a = 0; str[a] != '\0'; a++)
 ;
 a++;
-s = malloc(a * sizeof(*s));
+s = malloc(a *sizeof(*s));
 if (s == 0)
 return (NULL);
 for (b = 0; b < a; b++)
