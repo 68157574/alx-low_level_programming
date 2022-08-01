2b138decb33ff57560af2a22ee344ca32635a3f7
68157574 <abebemillion999@gmail.com>
2022-08-01 11:06:52 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 
 
 
@@ -16,6 +16,7 @@ float age;
 char *owner;
 };
 typedef struct dog dog_t;
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
