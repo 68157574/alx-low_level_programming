ef002a3bd41d862d7a5a420de755a8dedbf3decd
68157574 <abebemillion999@gmail.com>
2022-08-03 02:58:41 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,35 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - define function
-* @void - describe argument
-* Return: what does it return?
+* main - Print the opcodes of the function
+* @argc: Number of args given
+* @argv: Argument of number of bytes to print
+* Return: 0 on success, Print Error for errors, exit value 1 if
+* incorrect # of args, exit value 2 if number of bytes is negative
 */
-int main(void)
+int main(int argc, char *argv[])
 {
+char *add;
+int i, bytes;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+exit(2);
+}
+add = (char *)&main;
+bytes = atoi(argv[1]);
+i = 0;
+while (i < bytes - 1)
+{
+printf("%02hhx ", add[i]);
+i++;
+}
+printf("%02hhx\n", add[i]);
 return (0);
 }
