162467b91d53a00c4a9830865d704362f1068400
68157574 <abebemillion999@gmail.com>
2022-08-05 04:26:36 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,19 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-typedef struct validTypes
+#ifndef VARIADIC_FUNCS_H
+#define VARIADIC_FUNCS_H
+#define FORMAT(f) ("%"#f)
+
+/**
+* struct print_form - Define a struct to figure out how to print
+* @c: Character to check for
+* @f: Pointer to function to call
+*/
+typedef struct print_form
 {
-char *valid;
+char *c;
 void (*f)();
-} v_types;
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+} print_form_t;
 void print_all(const char * const format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+Void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
