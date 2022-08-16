6821b837df763ace3c310913964462b7f88a58c8
68157574 <abebemillion999@gmail.com>
2022-08-16 04:24:56 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,12 +1,23 @@
+#include <stdlib.h>
 #include "lists.h"
 
+
+
 /**
-* find_listint_loop - define function
-* @head:
-* Return: what does it return?
+* find_listint_loop - Find the loop inside a list
+* @head: Pointer to the start of a list
+* Return: Address of where the loop starts, NULL if
+* no loop is found
 */
 listint_t *find_listint_loop(listint_t *head)
 {
-head++;
-return (0);
+listint_t *holder;
+while (head != NULL)
+{
+holder = head;
+head = head->next;
+if (holder < head)
+return (head);
+}
+return (NULL);
 }
