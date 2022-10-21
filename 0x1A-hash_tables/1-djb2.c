92cf84df72bd662632d500cfb986e2e56aacc742
68157574 <abebemillion999@gmail.com>
2022-10-21 05:45:51 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,15 @@
 #include "hash_tables.h"
 
 /**
-* hash_djb2 - function that generates a random number through
-* a string given (hash number)
-* @str: string
-* Return: hash number
+* hash_djb2 - compute a hash using the djb2 algorithm
+* @str: a pointer to a string to hash
+* Return: the dbj2 hash of str
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash;
-int c;
-hash = 5381;
+unsigned long int hash = 5381;
+unsigned int c = '\0';
 while ((c = *str++))
-{
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-}
+hash += (hash << 5) + c;
 return (hash);
 }
